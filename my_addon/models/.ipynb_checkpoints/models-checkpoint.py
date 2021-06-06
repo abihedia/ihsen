345e1e4cec781a3df2ddc65dcb298d18a340ea37
Ihsen Zer <ihsen@gmail.com>
2021-06-06 00:09:33 +0000

    2test add my_addon module

--- my_addon/models/.ipynb_checkpoints/models-checkpoint.py
+++ my_addon/models/.ipynb_checkpoints/models-checkpoint.py
@@ -5,9 +5,10 @@ from odoo import models, fields, api
 
 class my_addon(models.Model):
     _inherit = 'res.partner'
-    _description = 'my_addon.my_addon'
+    
 
-    birthday = fields.Datetime(string='Date of birth')
+    birthday = fields.Datetime('Date of birth')
+    
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
--- my_addon/models/models.py
+++ my_addon/models/models.py
@@ -5,9 +5,8 @@ from odoo import models, fields, api
 
 class my_addon(models.Model):
     _inherit = 'res.partner'
-    _description = 'my_addon.my_addon'
-
-    birthday = fields.Datetime(string='Date of birth')
+    
+    birthday = fields.Datetime('Date of birth')
     
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
